964c137ea26b7428823ecf44268a019a1f71ab01
johnallen96 <john.r.allen96@gmail.com>
2020-01-05 19:37:55 +1000

    Major changes 3

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from projects.models import Project
 
 def project_index(request):
-    projects = Project.object.all()
+    projects = Project.objects.all()
     context = {
         'projects': projects
     }
